306365d1131dfccb757bd9cd05e3418f5f819133
Xelydor <53064016+Xelydor@users.noreply.github.com>
2021-05-26 15:25:44 -0500

    Avance

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'app/main.html')
+
+def procesar(request):
+    # val1 = request.GET["Palabra"]
+
+    return render(request, 'app/main.html')
